4aaa4b4a09a7696ff27b9bcb6ac5fec91e18771d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-02-18 10:37:18 -0500

    locked down all games pages used requireAuth function

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,24 @@
+// modules required for routing
 let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
+let passport = require('passport');
 
+// define the user model
+let UserModel = require('../models/users');
+let User = UserModel.User; // alias for User
+
+// define the game model
 let game = require('../models/games');
+
+// function to check if the user is authenticated
+function requireAuth(req, res, next) {
+  // check if the user is logged index
+  if(!req.isAuthenticated()) {
+    return res.redirect('auth/login');
+  }
+  next();
+}
 
 /* GET home page. wildcard */
 router.get('/', (req, res, next) => {
